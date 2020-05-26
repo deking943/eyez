35c9d11e2d979efa4e8b7374023ab83bb0d61b76
deking943 <mondayking943@gmail.com>
2020-05-26 13:20:33 +0100

    smart eyes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var balls = document.getElementsByClassName('ball');
 
-document.onmousemove = function(){
+const balls = document.getElementsByClassName('ball');
+
+document.onmousemove = () => {
+  
   //event.clientX => get the horizotal coordinate of the onmouseover
-  //event.clientY => get the vertical coordinate of the onmouseover
-  //window.innerWidth => get the browser width
+  const x = event.clientX * 100 / window.innerWidth +'%'; //window.innerWidth => get the browser width
 
-  var x = event.clientX * 100 / window.innerWidth +'%';
-  var y = event.clientY * 100 / window.innerHeight + '%';
+  //event.clientY => get the vertical coordinate of the onmouseover
+  const y = event.clientY * 100 / window.innerHeight + '%'; //window.innerHeihgt => get the browser height
 
   for(var i = 0; i<2; i++){
     balls[i].style.left = x;
